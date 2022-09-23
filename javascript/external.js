c9b343d4559b4d0a117e6ce083e4149b3ffd4c90
ryanfreed <rfreed@clarku.edu>
2022-09-23 12:24:13 -0400

    Completeed Task 3 variables

--- javascript/external.js
+++ javascript/external.js
@@ -1,4 +1,19 @@
 // document.write("Where is my JS Code?")
 // window.alert(5 + 6)
-document.write('<h1>A heading</h1>');
-document.write('<p>A sentence.</p>');
+// document.write('<h1>A heading</h1>');
+// document.write('<p>A sentence.</p>');
+
+// let x = "web";
+// let y = "mapping";
+// let out = x + y; 
+// document.write(out);
+
+let user_name = window.prompt("Please enter your name: ", 
+                              "type your name here");
+document.write(user_name);
+
+let sign = window.prompt();
+sign = prompt();
+sign = window.prompt('Are you feeling lucky');
+sign = window.prompt('Are you feeling lucky', 'sure');
+
