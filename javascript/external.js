ff43bf64675c299f50c0e06ec5d98a4d5ebe2d30
ryanfreed <rfreed@clarku.edu>
2022-09-28 12:28:17 -0400

    Saved user name

--- javascript/external.js
+++ javascript/external.js
@@ -8,12 +8,44 @@
 // let out = x + y; 
 // document.write(out);
 
-let user_name = window.prompt("Please enter your name: ", 
-                              "type your name here");
-document.write(user_name);
+// let user_name = window.prompt("Please enter your name: ", 
+//                              "type your name here");
+// document.write(user_name);
 
-let sign = window.prompt();
-sign = prompt();
-sign = window.prompt('Are you feeling lucky');
-sign = window.prompt('Are you feeling lucky', 'sure');
+// let sign = window.prompt();
+// sign = prompt();
+// sign = window.prompt('Are you feeling lucky');
+// sign = window.prompt('Are you feeling lucky', 'sure');
 
+// let x = 22;
+// let y = 33;
+// document.writeln(x + y);
+// document.writeln("<br>");
+// document.writeln(x += 33);
+
+// document.writeln("<button onclick='condition()'>Conditional Test </button>")
+
+// function condition() 
+// {
+//    let x = confirm("Are you sure you want to proceed?");
+
+//    if(x) {
+//        document.writeln("You chose Okay!");
+//    }
+
+//    else {
+//        document.writeln("You chose Cancel!");
+//    }
+// }
+
+// let arr = ["Joe", "April", "Mark", "Sara"];
+
+// for (var i=0; i < arr.length; i++)
+
+// {
+    // document.writeln(arr[i], "<br>");
+// }
+
+
+let askName = window.prompt("Please enter your name. ")
+document.write(askName)
