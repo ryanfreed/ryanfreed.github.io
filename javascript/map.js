60f80170cfe204584f13aa452057eda9e5392d77
ryanfreed <rfreed9@gmail.com>
2022-11-04 11:26:04 -0400

    started seven wonders section

--- javascript/map.js
+++ javascript/map.js
@@ -6,7 +6,8 @@ var options = {
     limit: 10
 };
 
-var control = L.Control.openCageSearch(options).addTo(mymap);
+/*var control = L.Control.openCageSearch(options).addTo(mymap);
+/*
 
 /* Tile Layer coming from Mapbox 
 
@@ -19,8 +20,14 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(mymap)
 
-/*
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+
+var tajMahal_marker = L.marker([27.174961, 78.042385]).addTo(mymap);
+var greatWall_marker = L.marker([40.334245, 116.477652]).addTo(mymap);
+var petra_marker = L.marker([30.328611, 35.441944]).addTo(mymap);
+var redeemer_marker = L.marker([-22.951389, -43.2108334]).addTo(mymap);
+var machuPicchu_marker = L.marker([-13.163056, -72.545556]).addTo(mymap);
+var chichenItza_marker = L.marker([20.682778, -88.569167]).addTo(mymap);
+var colosseum_marker = L.marker([41.890169, 12.492269]).addTo(mymap);
 
 var circle = L.circle([51.508, -0.11], { 
     color: 'red', 
@@ -56,7 +63,6 @@ polygon.on('click', function(e){
 marker.bindPopup("<b>Hello World!</b><br>I am a popup.").openPopup();
 circle.bindPopup("I am a circle.");
 polygon.bindPopup("I am a polygon.");
-*/
 
 var ZoomViewer = L.Control.extend({ 
     onAdd: function(){ 
